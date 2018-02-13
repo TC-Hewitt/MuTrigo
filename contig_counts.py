ae6989182806215edb23788c97728496a71f2aba
TC-Hewitt <timcharleshewitt@gmail.com>
2018-02-13 14:11:08 +1100

    updated to include N(25,50,75)

--- contig_counts.py
+++ contig_counts.py
@@ -36,8 +36,9 @@ def main():
         if args.show:
             print(ctgid.strip('>') + '\t' + str(len(tempSeq)) + '\n')
     	del(tempSeq)
-    #ctgLens.sort()
+    
     ctgLens.pop(0)
+    ctgLens.sort(reverse=True)
     ctgCount = len(ctgLens)
     totalLen = sum(ctgLens)
     ctgMax = max(ctgLens)
@@ -45,8 +46,29 @@ def main():
     ctgAvg = int(round(np.mean(ctgLens)))
     ctgMed = int(round(np.median(ctgLens)))
     ctgStd = round(np.std(ctgLens), 2)
+    len25 = totalLen/4
+    check25 = False
+    N25 = ''
+    len50 = len25*2
+    check50 = False
+    N50 = ''
+    len75 = len25*3
+    N75 = ''
+    rollSum = 0
+    for num in ctgLens:
+        rollSum += num
+        if check25 == False and rollSum >= len25:
+            N25 = str(num)
+            check25 = True
+        if check50 == False and rollSum >= len50:
+            N50 = str(num)
+            check50 = True
+        if rollSum >= len75:
+            N75 = str(num)
+            break
 
-    print('for ' + args.input + ':\nnumber of contigs = ' + str(ctgCount) + '\ncombined length = ' + str(totalLen) + 'bp\nmax length = ' + str(ctgMax) + 'bp\nmin length = ' + str(ctgMin) + 'bp\naverage length = ' + str(ctgAvg) + 'bp\nmedian length = ' + str(ctgMed) + 'bp\nSD = ' + str(ctgStd) + '\nNs = ' + str(Ns) + '\n')
+
+    print('for ' + args.input + ':\nnumber of contigs = ' + str(ctgCount) + '\ncombined length = ' + str(totalLen) + 'bp\nmax length = ' + str(ctgMax) + 'bp\nmin length = ' + str(ctgMin) + 'bp\naverage length = ' + str(ctgAvg) + 'bp\nmedian length = ' + str(ctgMed) + 'bp\nSD = ' + str(ctgStd) + '\nNs = ' + str(Ns) + '\nN25 = ' + N25 + '\nN50 = ' + N50 + '\nN75 = ' + N75 + '\n')
 
 if __name__ == '__main__':
     main()
